85e289ade377e38a8e90421383f528318eedd4f1
ljdongs <ljdongs@126.com>
2019-05-22 09:34:09 +0800

    main.js修改提交

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    // inputs是对象
+    let distance = inputs.distance
+    let parktime = inputs.parkTime
+    if(distance < 2)
+    {return parse.Number(6 + 0.25 * parktime + 0.8 * distance)}
+    else if(distance >= 2 && distance < 8) {
+       return parse.Number(0.8 * distance + 0.25 * parktime)
+    }
+    else{
+      return parse.Number((0.8 + 0.8 * 50%) * distance + parktime * 0.25)
+    }
 };
